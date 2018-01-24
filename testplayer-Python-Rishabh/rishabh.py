18d18993db3ada930cbb030daa5c144da5276cc3
Rishabhk2357 <rishabh.krishnan@gmail.com>
2018-01-23 19:31:02 -0500

    Changed up knight behavior

--- testplayer-Python-Rishabh/rishabh.py
+++ testplayer-Python-Rishabh/rishabh.py
@@ -4,7 +4,7 @@ rally_loc=None
 ####More Factory Code
 nearby = gc.sense_nearby_units_by_team(location.map_location(),30,other_team);
 minDistance=0
-minUnit=Unit()
+minUnit=None
 attacked=False
 for other in nearby:
     if gc.is_attack_ready(unit.id) and gc.can_attack(unit.id, other.id):
@@ -13,36 +13,36 @@ for other in nearby:
         attacked=True
         return
     else:
-    	tempDistance=location.map_location().distance_squared_to(other.location.map_location())
-    	if tempDistance<minDistance or minDistance==0:
-    		minDistance=tempDistance
-    		minUnit=other
+        tempDistance=location.map_location().distance_squared_to(other.location.map_location())
+        if tempDistance<minDistance or minDistance==0:
+            minDistance=tempDistance
+            minUnit=other
 if(len(nearby)>0):
     tempDir=location.map_location().direction_to(minUnit.location.map_location())
-    if gc.can_move(unit.id,tempDir):
-        gc.move(unit.id,tempDir)
+    if gc.is_move_ready(unit.id) and gc.can_move(unit.id,tempDir):
+        gc.move_robot(unit.id,tempDir)
 else:
-	nearby_rockets=gc.sense_nearby_units_by_type(location.map_location(), 10, gc.UnitType.Rocket)
-	minRocket=None
-	minDistance=0
-	for rocket in nearby_rockets:
-		tempDist=location.map_location().distance_squared_to(rocket.location.map_location())
-		if rocket.team==my_team and tempDist<minDistance and len(rocket.structure_garrison()<8):
-			minRocket=rocket
-			minDistance=tempDist
-	if len(nearby_rockets)>0:
-		tempDir=location.map_location().direction_to(minRocket.location.map_location())
-	    if gc.can_move(unit.id,tempDir):
-	        gc.move(unit.id,tempDir)
-	else:
-		if (not rally_loc==None):
-			direc=location.map_location().direction_to(rally_loc)
-			if gc.can_move(unit.id,direc):
-				gc.move(unit.id,direc)
-		else:
-			d=random.choice(directions)
-			if gc.can_move(unit.id,d):
-				gc.move(unit.id,d)
+    nearby_rockets=gc.sense_nearby_units_by_type(location.map_location(), 10, bc.UnitType.Rocket)
+    minRocket=None
+    minDistance=0
+    for rocket in nearby_rockets:
+        tempDist=location.map_location().distance_squared_to(rocket.location.map_location())
+        if rocket.team==my_team and (tempDist<minDistance or minDistance==0) and len(rocket.structure_garrison())<8 and not rocket.rocket_is_used():
+            minRocket=rocket
+            minDistance=tempDist
+    if minRocket is not None:
+        tempDir=location.map_location().direction_to(minRocket.location.map_location())
+        if gc.is_move_ready(unit.id) and gc.can_move(unit.id,tempDir):
+            gc.move_robot(unit.id,tempDir)
+    else:
+        if (not rally_loc==None):
+            direc=location.map_location().direction_to(rally_loc)
+            if gc.can_move(unit.id,direc):
+                gc.move(unit.id,direc)
+        else:
+            d=random.choice(directions)
+            if gc.is_move_ready(unit.id) and gc.can_move(unit.id,d):
+                gc.move_robot(unit.id,d)
 
 
 	                   
--- testplayer-Python-Rishabh/run.py
+++ testplayer-Python-Rishabh/run.py
@@ -7,7 +7,7 @@ import os
 print(os.getcwd())
 
 print("pystarting")
-
+rally_loc=None
 # A GameController is the main type that you talk to the game with.
 # Its constructor will connect to a running game.
 gc = bc.GameController()
@@ -121,12 +121,47 @@ def worker(unit, location, my_planet):
                     return
         ######################## end of knight code
 def knight(unit, location, my_planet):
-    nearby = gc.sense_nearby_units_by_team(location.map_location(), 70, other_team);
+    nearby = gc.sense_nearby_units_by_team(location.map_location(),30,other_team);
+    minDistance=0
+    minUnit=None
+    attacked=False
     for other in nearby:
         if gc.is_attack_ready(unit.id) and gc.can_attack(unit.id, other.id):
             print('attacked a thing!')
             gc.attack(unit.id, other.id)
+            attacked=True
             return
+        else:
+            tempDistance=location.map_location().distance_squared_to(other.location.map_location())
+            if tempDistance<minDistance or minDistance==0:
+                minDistance=tempDistance
+                minUnit=other
+    if(len(nearby)>0):
+        tempDir=location.map_location().direction_to(minUnit.location.map_location())
+        if gc.is_move_ready(unit.id) and gc.can_move(unit.id,tempDir):
+            gc.move_robot(unit.id,tempDir)
+    else:
+        nearby_rockets=gc.sense_nearby_units_by_type(location.map_location(), 10, bc.UnitType.Rocket)
+        minRocket=None
+        minDistance=0
+        for rocket in nearby_rockets:
+            tempDist=location.map_location().distance_squared_to(rocket.location.map_location())
+            if rocket.team==my_team and (tempDist<minDistance or minDistance==0) and len(rocket.structure_garrison())<8 and not rocket.rocket_is_used():
+                minRocket=rocket
+                minDistance=tempDist
+        if minRocket is not None:
+            tempDir=location.map_location().direction_to(minRocket.location.map_location())
+            if gc.is_move_ready(unit.id) and gc.can_move(unit.id,tempDir):
+                gc.move_robot(unit.id,tempDir)
+        else:
+            if (not rally_loc==None):
+                direc=location.map_location().direction_to(rally_loc)
+                if gc.can_move(unit.id,direc):
+                    gc.move(unit.id,direc)
+            else:
+                d=random.choice(directions)
+                if gc.is_move_ready(unit.id) and gc.can_move(unit.id,d):
+                    gc.move_robot(unit.id,d)
 
 def main():
     for unit in gc.my_units():
